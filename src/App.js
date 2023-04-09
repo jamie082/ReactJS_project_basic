71a4767103ec7dfbaa26d98bb9e390db0547c0a0
Jamie Morrissey <jamie_morrissey@Jamies-MacBook-Air-2.local>
2023-04-09 00:21:23 -0700

    added list source

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import myImage from './reactjs.jpeg'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <p>This is a simple paragraph</p>
+      <ol>
+        <li>C</li>
+        <li>Python</li>
+        <li>JavaScript</li>
+      </ol>
       <img src={myImage}></img>
     </div>
   );
 }
 
-function console_code() {
-  var numbers = [1,2,3,4,5];
-  const multiplyNums = numbers.map((number)=>{
+var numbers = [1,2,3,4,5];
+const multiplyNums = numbers.map((number)=>{
     return (number * 5);
 });
 
 console.log(multiplyNums);
+
+const numbers_2 = [1,2,3,4,5];
+
+const updatedNums = numbers_2.map((number)=>{
+  return <li>{number}</li>;
+});
 
 export default App;
